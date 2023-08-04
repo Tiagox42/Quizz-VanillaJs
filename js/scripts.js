8ffa5fd71d6bdeaacd5b7d5174e94875efde698a
Tiago Dantas <tiagodantas42@gmail.com>
2023-08-04 12:04:22 -0300

    finally

--- js/scripts.js
+++ js/scripts.js
@@ -1,107 +1,105 @@
 // Declaração de variáveis
-
 const question = document.querySelector("#question");
 const answersBox = document.querySelector("#answers-box");
 const quizzContainer = document.querySelector("#quizz-container");
 const scoreContainer = document.querySelector("#score-container");
-const letters = ["a", "b", "c", "d"];
+const letters = ['a', 'b', 'c', 'd'];
 let points = 0;
 let actualQuestion = 0;
 
 // Perguntas
 const questions = [
-    {
-      "question": "PHP foi desenvolvido para qual fim?",
-      "answers": [
-        {
-          "answer": "back-end",
-          "correct": true
-        },
-        {
-          "answer": "front-end",
-          "correct": false
-        },
-        {
-          "answer": "Sistema operacional",
-          "correct": false
-        },
-        {
-          "answer": "Banco de dados",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "Uma forma de declarar variável em JavaScript:",
-      "answers": [
-        {
-          "answer": "$var",
-          "correct": false
-        },
-        {
-          "answer": "var",
-          "correct": true
-        },
-        {
-          "answer": "@var",
-          "correct": false
-        },
-        {
-          "answer": "#let",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "Qual o seletor de id no CSS?",
-      "answers": [
-        {
-          "answer": "#",
-          "correct": true
-        },
-        {
-          "answer": ".",
-          "correct": false
-        },
-        {
-          "answer": "@",
-          "correct": false
-        },
-        {
-          "answer": "/",
-          "correct": false
-        },
-      ]
-    },
-  ];
+  {
+    "question": "PHP foi desenvolvido para qual fim?",
+    "answers": [
+      {
+        "answer": "back-end",
+        "correct": true
+      },
+      {
+        "answer": "front-end",
+        "correct": false
+      },
+      {
+        "answer": "Sistema operacional",
+        "correct": false
+      },
+      {
+        "answer": "Banco de dados",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Uma forma de declarar variável em JavaScript:",
+    "answers": [
+      {
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
+        "correct": true
+      },
+      {
+        "answer": "@var",
+        "correct": false
+      },
+      {
+        "answer": "#let",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Qual o seletor de id no CSS?",
+    "answers": [
+      {
+        "answer": "#",
+        "correct": true
+      },
+      {
+        "answer": ".",
+        "correct": false
+      },
+      {
+        "answer": "@",
+        "correct": false
+      },
+      {
+        "answer": "/",
+        "correct": false
+      },
+    ]
+  },
+]
 
-// Substituição do quizz para a primeira pergunta
+// Substituição do layout pela primeira questão
 function init() {
-    createQuestion(0);
+  createQuestion(0)
 }
 
-// Cria uma pergunta
+//criar questão 
 function createQuestion(i) {
-  
-  // Limpa a questão anterior
+
+  //limpar questão anterior
   const oldButtons = answersBox.querySelectorAll("button");
 
   oldButtons.forEach(function(btn) {
     btn.remove();
   });
 
-  // Alterar o texto da pergunta
+  // Altera texto da pergunta
   const questionText = question.querySelector("#question-text");
   const questionNumber = question.querySelector("#question-number");
-
 
   questionText.textContent = questions[i].question;
   questionNumber.textContent = i + 1;
 
-  // Insere as alternativas
+  // Insere alternativas
   questions[i].answers.forEach(function(answer, i) {
-
-    // Cria o template do botão do quizz
+    
+    // Altera texto do template
     const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
 
     const letterBtn = answerTemplate.querySelector(".btn-letter");
@@ -112,74 +110,107 @@ function createQuestion(i) {
 
     answerTemplate.setAttribute("correct-answer", answer["correct"]);
 
-    // Remover Hide e template class
+    // remove classe de hide e template do template
     answerTemplate.classList.remove("hide");
     answerTemplate.classList.remove("answer-template");
 
-    //Inserir a alternativa na tela
+    // Insere template na tela
     answersBox.appendChild(answerTemplate);
-
-    // Evento de click
-    answerTemplate.addEventListener("click", function() {
-      checkAnswer(this);
-    })
 
   });
 
-  // Incrementa número da questão
+  // Cria evento em todos os botões
+  const buttons = answersBox.querySelectorAll("button");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      checkAnswer(this, buttons);
+    });
+  });
+
+  // Incrementa o número atual de questões
   actualQuestion++;
 
 }
 
-// verificando resposta
-function checkAnswer(btn) {
-
-  // selecionar todos os botões
-  const buttons = answersBox.querySelectorAll("button");
-
-  //verifica se a resposta está correta e adiciona classes nos botões
+// Verificando se resposta está correta
+function checkAnswer(btn, buttons) {
+  
+  // Exibir respostas erradas e a certa
   buttons.forEach(function(button) {
+
     if(button.getAttribute("correct-answer") === "true") {
-
       button.classList.add("correct-answer");
-
-      // verifica se acertou
+      // checa se o usuário acertou
       if(btn === button) {
-        //incremento
+        // incrementa os pontos
         points++;
       }
-
     } else {
-
       button.classList.add("wrong-answer");
-
     }
 
   });
 
-  //Exibir próxima pergunta
   nextQuestion();
 
 }
 
-//Exibe a próxima pergunta do quizz
+// Exibe a próxima pergunta
 function nextQuestion() {
 
-  //timer para usuário ver as respostas
+  // Timer para ver se acertou ou errou
   setTimeout(function() {
-    
-    //verifica se ainda há perguntas
+
+    // checa se ainda há mais perguntas
     if(actualQuestion >= questions.length) {
-    // apresenta a msg de sucesso
+      // apresenta msg de sucesso
+      showSuccessMessage();
+      return;
     }
 
     createQuestion(actualQuestion)
 
-  }, 1500);
+  }, 1000);
 
 }
 
+// Tela final
+function showSuccessMessage() {
 
-// Inicia Quiz
+  hideOrShowQuizz();
+
+  //calcula o score
+  const score = ((points / questions.length) * 100).toFixed(2);
+  const scoreDisplay = document.querySelector("#display-score span");
+
+  scoreDisplay.textContent = score.toString();
+
+  //alterar número de perguntas corretas
+  const correctAnswers = document.querySelector("#correct-answers");
+  correctAnswers.textContent = points;
+
+  //alterar total de perguntas
+  const totalQuestions = document.querySelector("#questions-qty");
+  totalQuestions.textContent = questions.length;
+
+}
+
+//reinicia o game
+const restartBtn = document.querySelector("#restart");
+
+restartBtn.addEventListener("click", function() {
+  actualQuestion = 0;
+  points = 0;
+  hideOrShowQuizz();
+  init();
+});
+
+//mostra ou exibe o quizz
+function hideOrShowQuizz() {
+  quizzContainer.classList.toggle("hide");
+  scoreContainer.classList.toggle("hide");
+}
+
+//inicialização
 init();
-
